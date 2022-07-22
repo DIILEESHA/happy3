4c6fd2a371c3c7a616218a280aa0f801d6ca6df2
Dileesha Lakshan <89340276+DIILEESHA@users.noreply.github.com>
2022-07-22 23:52:57 +0530

    Add footer

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Footer from '../footer/Footer'
 import Navbar from "../navbar/Navbar"
 import Project from "../project/Project"
 import './projects.css'
@@ -7,7 +8,7 @@ export default function Projects() {
   return (
     <div className='projects'><Navbar/>
     <Project/>
- 
+ <Footer/>
     </div>
   )
 }
